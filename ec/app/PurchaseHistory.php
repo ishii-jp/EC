30630faf353954f6d3bbc5812f5d3265e977ab80
KunitakaIshii <sadaharu5goo@icloud.com>
2021-03-04 22:02:22 +0900

    フォーマッター

--- ec/app/PurchaseHistory.php
+++ ec/app/PurchaseHistory.php
@@ -22,6 +22,7 @@ class PurchaseHistory extends Model
 
     /**
      * ログインしているユーザーの購入履歴を取得し返します。
+     * 
      * @param int $userId
      * @param string|array リレーションデータの Eager Loadingするために指定する、初期値はgood。複数指定する場合は配列で指定
      * @return array 購入履歴
@@ -33,6 +34,7 @@ class PurchaseHistory extends Model
 
     /**
      * 購入履歴を登録する
+     * 
      * @param array $items 購入した商品
      * @param int $userId ログインしているユーザーid
      */
