4e3bc3e303281795dada51ed763fe398b51e2aad
KunitakaIshii <sadaharu5goo@icloud.com>
2022-01-27 15:34:43 +0900

    トップ画面に注意書きを追加しました

--- ec/resources/views/ec/top.blade.php
+++ ec/resources/views/ec/top.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
+    <div class="alert alert-info" role="alert">
+        これは学習用のサイトです。 本物のECサイトではありません。<br> This is a study site. No EC site.
+    </div>
+
     <p>トップ画面</p>
     <p><a href="{{ route('show') }}"><button class="btn btn-primary btn-sm" type="button">商品一覧</button></a></p>
 </div>
